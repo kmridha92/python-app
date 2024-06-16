f0f487f23dc6004b1038ca5ab6c4441f99ca5f8d
Kathak Mridha <kmridha92@gmail.com>
2024-06-16 13:20:49 +1000

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM python:3.10.14-alpine3.20
 
-WORKDIR /python-app
+WORKDIR /app
 
-COPY requirements.txt /python-app
+COPY requirements.txt /app
 
 RUN pip install -r requirements.txt --no-cache-dir
 
-COPY . /python-app
+COPY . /app
 
 CMD python app.py
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def helloWorld():
     return "Hello World"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get("PORT", 1234))
+    app.run(host='0.0.0.0', port=port, debug=True)
